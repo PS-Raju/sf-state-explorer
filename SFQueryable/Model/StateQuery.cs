7343bce0d5653683a019546e0ff75e1b0721dea5
Srinivas Pakalapati <srpakala@microsoft.com>
2019-07-22 19:34:20 +0530

    Added basic query object

---
+++ SFQueryable/Model/StateQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace SFQuerable.Model
+{
+    public class StateQuery
+    {
+        // Mandate parameter.
+        public string ServiceName { get; set; }
+
+        // Optional, if porvided will query this partition else all partitions.
+        public string PartitionKey { get; set; }
+
+        // Types could be "ReliableDictionary", "ReliableQueue", "ReliableConcurrentQueue"
+        public string CollectionType { get; set; }
+
+        // Name of collection
+        public string ReliableStateName { get; set; }
+
+        // In case if above state is a dictioanry, use this key to query the value.
+        public string Key { get; set; }
+    }
+}
