1f2b29ddc70b221a14672d3e5804a2d23cc376d3
SaravananThangavel27 <saravananilango@gmail.com>
2019-07-23 13:57:37 +0530

    Add the ReliableStateMetadata model definition

--- SFQueryable/Model/ReliableStateMetadata.cs
+++ SFQueryable/Model/ReliableStateMetadata.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFQuerable.Model
+﻿namespace SFQuerable.Model
 {
-    class ReliableStateMetadata
+    /// <summary>
+    /// Contians the metadata for the IReliableState instances.
+    /// </summary>
+    public class ReliableStateMetadata
     {
+        /// <summary>
+        /// Gets the unique name for the IReliableState.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the type of the IReliableState (like IReliableConcurrentQueue,  IReliableDictionary, etc).
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets the number of elements contained in the IReliableState.
+        /// </summary>
+        public long Count { get; set; }
     }
 }
